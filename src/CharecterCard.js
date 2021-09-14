b427fb036ce76cc42a3c78b6661de5c3064ba696
salmabutlam01 <s613552052@phuket.psu.ac.th>
2021-09-15 00:29:53 +0700

    test2

---
+++ src/CharecterCard.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+export default function CharacterCard(props) {
+    const [active, setActive] = useState(false);
+    const activate = () => {
+        if (!active) {
+            setActive(true)
+            props.activationHandler(props.value)
+        } else
+            setActive(false)
+    }
+
+    const className = `card ${active ? 'activeCard': ''}`
+
+    return (
+        <div className={className} onClick={activate}>{ props.value}</div>
+    )
+}
