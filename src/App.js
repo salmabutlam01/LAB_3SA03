ebee10a0c72ddb975286036167df7706b737c619
salmabutlam01 <s613552052@phuket.psu.ac.th>
2021-09-23 00:13:08 +0700

    Finalll

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
-import WordCard from "./WordCard";
+import React from 'react';
 import './App.css';
-const word = "Hello";
-function App()
-{
-  return (
-    <div>
-          <a><h1>  HELLO GAME  </h1></a>
-          <p>
-              Advance Lab3SA03 Salma Butlam 6135512052
-          </p>
-          <WordCard value="hello"/>
-    </div>
-  );
-}
-export default App;
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+import Timer from './Timer';
+
+
+  function App(){
+    const [seconds, setSeconds] = React.useState(10);
+    React.useEffect(() => {
+      console.log(<Timer initialMinute={0} initialSeconds={0}/>)
+      if (seconds > 0) {
+        setTimeout(() => setSeconds(seconds - 1), 1000);
+      } else {
+        setSeconds('BOOOOM!');
+      }
+    },)
+    return(
+      <div>
+         <a><h1>  HELLO GAME  </h1></a>
+         <p>
+             Lab_3SA03 Salma Butlam 6135512052
+         </p>
+      <div>
+        {seconds}
+      </div>
+        <WordCard value="Hello" />
+      </div>
+ );}
+ export default App;
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,61 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import CharacterCard from './CharecterCard'; 
+import React,{useState,useEffect} from 'react';
 import _ from 'lodash';
+import CharacterCard from './CharacterCard'; 
 
-const prepareStateFromWord = (given_word) => {     
-    let word = given_word.toUpperCase()     
-    let chars = _.shuffle(Array.from(word))     
-    return {       
-        word,       
-        chars,       
-        attempt: 1,       
-        guess: '',       
-        completed: false     
-    } 
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+    word,
+    chars,
+    attempt: 1,
+    guess: '',
+    completed: false
+    }
 }
-
-export default function WordCard(props){
+export default function WordCard(props) { 
     const [state, setState] = useState({
-        word: '',       
-        chars: '',       
-        attempt: 1,       
-        guess: '',       
-        completed: false 
-    })
+        word: '',
+        chars: '',
+        attempt: 1,
+        guess: '',
+        completed: false
+    }) 
     useEffect(() => {
         let data = prepareStateFromWord(props.value);
         setState({
             ...state,
-            word: data.word,      
-            chars: data.chars,       
-            attempt: data.attempt,       
-            guess: data.guess,       
-            completed: data.completed 
+            word: data.word,
+            chars: data.chars,
+            attempt: data.attempt,
+            guess: data.guess,
+            completed: data.completed
         })
-    }
-    )
-    const activationHandler = (c) => {     
-        console.log(`${c} has been activated.`)      
-        let guess = state.guess + c     
-        setState({...state, guess})          
-        
-        if(guess.length == state.word.length){             
-            if(guess == state.word){         
-                console.log('yeah!')         
-                setState({...state, guess: '', completed: true})       
-            }else{         
-                console.log('reset')         
-                setState({...state, guess: '', attempt: state.attempt + 1})       
-            }     
-        }       
-    } 
+    },[])
+    const activationHandler = (c) => {
+        console.log(`${c} has been activated.`)
+        let guess = state.guess + c
+        setState({...state, guess})
+        if(guess.length == state.word.length){
+             if(guess == state.word){
+                console.log('yeah!')
+                setState({...state, guess: '', completed: true})
+            }else{
+                 console.log('reset')
+                 setState({...state, guess: '', attempt: state.attempt + 1})
+            }
+        }
+    }   
     return (
-          <div> 
-            {
-             Array.from(props.value).map(
-                 (c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler}
-                 attempt={state.attempt}/>) 
-                }
-         </div>
+        <div>
+            {Array.from(state.chars).map((c,i) => <CharacterCard value={c}
+            activationHandler= {activationHandler}
+            attempt={state.attempt}/> )}
+        </div>      
     );
-}
+        }
